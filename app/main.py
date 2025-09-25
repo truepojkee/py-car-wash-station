9b9dde4739434971699984e1258a6bb950fc4b80
Mykhailo Shpyt <cbo6oda22@gmail.com>
2025-09-25 14:05:32 +0200

    implemented CarWashStation and Car class with methods

--- app/main.py
+++ app/main.py
@@ -1,8 +1,57 @@
 class Car:
-    # write your code here
-    pass
+
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+
+        if comfort_class < 1 or comfort_class > 7:
+            raise ValueError("comfort_class must be between 1 and 7")
+        if clean_mark < 1 or clean_mark > 10:
+            raise ValueError("clean_mark must be between 1 and 10")
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+
+    def __init__(self,
+                 distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int):
+        if not 1 <= clean_power <= 10:
+            raise ValueError("clean_power must be between 1 and 10")
+        if not (1.0 <= distance_from_city_center <= 10.0):
+            raise ValueError("distance_from_city_center must be between 1.0 and 10.0")
+        self.distance_from_city_center = float(distance_from_city_center)
+        self.clean_power = clean_power
+        self.average_rating = round(float(average_rating), 1)
+        self.count_of_ratings = count_of_ratings
+
+
+    def serve_cars(self, cars: list):
+
+        income = 0.0
+
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return round(income, 1)
+
+    def calculate_washing_price(self, car):
+        diff = self.clean_power - car.clean_mark
+        if diff <= 0:
+            return 0.0
+        price = car.comfort_class * diff * self.average_rating / self.distance_from_city_center
+        return round(price, 1)
+
+    def wash_single_car(self, car):
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rate):
+        if not (1.0 <= float(rate) <= 5.0):
+            raise ValueError("rate must be between 1.0 and 5.0")
+        new_average = (self.average_rating * self.count_of_ratings +
+                       float(rate)) / (
+                self.count_of_ratings + 1)
+        self.average_rating = round(new_average, 1)
+        self.count_of_ratings += 1
